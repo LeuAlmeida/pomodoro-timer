9b4ababde7e0477401b8ba457a5fb1c41cbbf901
Léu Almeida <leo@webid.net.br>
2020-05-05 19:55:46 -0300

    style(SquareButton): implementing styles

--- src/components/Button/SquareButton/styles.js
+++ src/components/Button/SquareButton/styles.js
@@ -1,5 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
-  
+export const Button = styled.button.attrs({
+  type: "button",
+})`
+  display: flex;
+  margin-top: 80px;
+
+  border-radius: 8px;
+  width: 55px;
+  height: 55px;
+
+  box-shadow: inset 1px 2px #ffffff0a, -4px -4px 20px -5px #ffffff59, 2px 2px 20px #000000b5;
 `;

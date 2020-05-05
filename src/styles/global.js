2b475db3f415f0216aefbf7670958bbf6b9edeb2
Léu Almeida <leo@webid.net.br>
2020-05-05 17:43:55 -0300

    style(global): improving button styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap');
+
  * {
    margin: 0;
    padding: 0;
@@ -10,11 +13,36 @@ export default createGlobalStyle`
 
  html, body, #root {
    min-height: 100%;
+
+   font-family: 'Open Sans Condensed', sans-serif;
+   color: #9ca1bc;
+   font-weight: 300;
  }
 
  body {
+  max-width: 500px;
+  margin: 0 auto;
+
   background: rgb(70,75,104);
   background: linear-gradient(90deg, rgba(70,75,104,1) 0%, rgba(41,44,68,1) 100%);
    -webkit-font-smoothing: antialiased !important;
  }
+
+ button {
+   background: transparent;
+   cursor: pointer;
+   border: 0;
+   transition: opacity 0.3s;
+
+   display: flex;
+  justify-content: center;
+  align-items: center;
+
+  box-shadow: -4px -4px 20px -5px #ffffff38, 2px 2px 20px #00000078;
+
+   &:hover {
+     opacity: 0.6;
+   }
+ }
+ 
 `;

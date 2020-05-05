f3c910ef227fb2bc86db539eaffdf12e46362ddd
Léu Almeida <leo@webid.net.br>
2020-05-05 17:44:57 -0300

    style(Header): improving Header styles

---
+++ src/components/Header/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 24px;
+
+  padding-top: 50px;
+`;
+
+export const Title = styled.span`
+  font-size: 32px;
+`;
+
+export const Button = styled.button``;

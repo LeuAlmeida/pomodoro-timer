afb4cb099ffe52dcd819fb59a04d7ac900b20ba2
Léu Almeida <leo@webid.net.br>
2020-05-05 19:55:56 -0300

    feat(SquareButton): improving content structure

--- src/components/Button/SquareButton/index.js
+++ src/components/Button/SquareButton/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-// import { Container } from './styles';
+import { FiPlay, FiPause } from 'react-icons/fi'
 
-function SquareButton() {
-  return <div />;
+import { Button } from './styles';
+
+function SquareButton({ play, onClick }) {
+  return (
+    <>
+    <Button onClick={onClick}>
+      {play && <FiPlay color="#9ca1bc" size={26} />}
+      {!play && <FiPause color="#9ca1bc" size={26} />}
+      </Button>
+    </>
+  );
 }
 
 export default SquareButton;

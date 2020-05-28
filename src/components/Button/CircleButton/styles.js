c5842ec3f947c7c59eb8364ba6ed79cc338c47b4
Léu Almeida <leo@webid.net.br>
2020-05-28 17:20:15 -0300

    style(CircleButton): create label component

--- src/components/Button/CircleButton/styles.js
+++ src/components/Button/CircleButton/styles.js
@@ -7,3 +7,10 @@ export const Button = styled.button.attrs({
   width: 40px;
   height: 40px;
 `;
+
+export const Label = styled.label`
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`;

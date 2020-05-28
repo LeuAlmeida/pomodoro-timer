7688e6e5e1b3253ec213809be1dcf6d3f59fb13c
Léu Almeida <leo@webid.net.br>
2020-05-28 15:50:21 -0300

    feat(Main): implementing some functionalities to main file

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Mousetrap from "mousetrap";
 import {
   CircularProgressbarWithChildren,
   buildStyles,
 } from "react-circular-progressbar";
-import { addMinutes } from "date-fns";
-
-import "react-circular-progressbar/dist/styles.css";
 
 import Header from "../../components/Header";
 import SquareButton from "../../components/Button/SquareButton";
@@ -22,88 +20,234 @@ import {
   PlayStopContent,
 } from "./styles";
 
-function Main() {
-  const [minutes, setMinutes] = useState(25);
+export default class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: 0,
+      play: false,
+      timeType: 0,
+      title: "",
+    };
+    // Bind early, avoid function creation on render loop
+    this.setTimeForCode = this.setTime.bind(this, 1500);
+    this.setTimeForSocial = this.setTime.bind(this, 300);
+    this.setTimeForCoffee = this.setTime.bind(this, 900);
+    this.reset = this.reset.bind(this);
+    this.play = this.play.bind(this);
+    this.elapseTime = this.elapseTime.bind(this);
+  }
 
-  const [play, setPlay] = useState(true);
-  const [maxValue, setmaxValue] = useState(25);
+  componentDidMount() {
+    this.setDefaultTime();
+    this.startShortcuts();
+    Notification.requestPermission();
+  }
 
-  const [finalTime, setFinalTime] = useState(null);
+  elapseTime() {
+    if (this.state.time === 0) {
+      this.reset(0);
+      this.alert();
+    }
+    if (this.state.play === true) {
+      let newState = this.state.time - 1;
+      this.setState({ time: newState, title: this.getTitle(newState) });
+    }
+  }
 
-  var min;
-  var counter;
+  format(seconds) {
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = Math.floor((seconds % 3600) % 60);
+    let timeFormated = (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s;
+    return timeFormated;
+  }
 
-  function handleStart() {
-    setFinalTime(addMinutes(new Date(), maxValue % minutes));
-    setPlay(!play);
+  getFormatTypes() {
+    return [
+      { type: "work", time: 1500 },
+      { type: "relax", time: 300 },
+      { type: "coffee", time: 900 },
+    ];
+  }
 
-    var deadline = addMinutes(new Date(), finalTime || minutes).getTime();
-
-    counter = setInterval(() => {
-      var now = new Date().getTime();
-      var t = deadline - now;
-      min = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-
-      setMinutes(min);
-
-      if (t < 0) {
-        setMinutes(0);
-        console.log("Acabou!!!");
+  formatType(timeType) {
+    let timeTypes = this.getFormatTypes();
+    for (let i = 0; i < timeTypes.length; i++) {
+      let timeObj = timeTypes[i];
+      if (timeObj.time === timeType) {
+        return timeObj.type;
       }
-
-      console.log(`Minutos: ${min}`)
-    }, 1000);
-    console.log('Play!');
+    }
+    return null;
   }
 
-  function handlePause() {
-    console.log('Pause!')
-    setFinalTime(null);
-    setPlay(!play);
-    clearInterval(counter);
+  restartInterval() {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.elapseTime, 1000);
   }
 
-  useEffect(() => {}, []);
+  play() {
+    if (true === this.state.play) return;
 
-  return (
-    <Container>
-      <Header title="Pomodoro" sound alert />
+    this.restartInterval();
 
-      <CounterContainer>
-        <CircularProgressbarWithChildren
-          maxValue={maxValue}
-          strokeWidth={3}
-          value={minutes}
-          styles={buildStyles({
-            rotation: 1,
-            strokeLinecap: "round",
-            pathTransitionDuration: 0.5,
-            pathColor: "#7457f2",
-            textColor: "#f88",
-            trailColor: "#3c405e",
-            backgroundColor: "#7457f2",
-          })}
-        >
-          <TimeNum>{minutes}</TimeNum>
-          <br />
-          <TimeText>Minutes</TimeText>
-        </CircularProgressbarWithChildren>
-      </CounterContainer>
+    this.setState({
+      play: true,
+    });
+  }
 
-      <ContentContainer>
-        <ContentTitle>Work</ContentTitle>
-        <ActionButtonsContainer type="work">
-          <ActionButton active />
-          <ActionButton />
-          <ActionButton />
-        </ActionButtonsContainer>
-      </ContentContainer>
+  reset(resetFor = this.state.time) {
+    clearInterval(this.interval);
+    let time = this.format(resetFor);
+    this.setState({ play: false });
+  }
 
-      <PlayStopContent>
-        <SquareButton onClick={play ? handleStart : handlePause} play={play} />
-      </PlayStopContent>
-    </Container>
-  );
+  togglePlay() {
+    if (true === this.state.play) return this.reset();
+
+    return this.play();
+  }
+
+  setTime(newTime) {
+    this.restartInterval();
+
+    this.setState({
+      time: newTime,
+      timeType: newTime,
+      title: this.getTitle(newTime),
+      play: true,
+    });
+  }
+
+  setDefaultTime() {
+    let defaultTime = 1500;
+
+    this.setState({
+      time: defaultTime,
+      timeType: defaultTime,
+      title: this.getTitle(defaultTime),
+      play: false,
+    });
+  }
+
+  getTitle(time) {
+    time = typeof time === "undefined" ? this.state.time : time;
+    let _title = this.format(time) + " | Pomodoro timer";
+    return _title;
+  }
+
+  startShortcuts() {
+    Mousetrap.bind("space", this.togglePlay.bind(this));
+    Mousetrap.bind(["ctrl+left", "meta+left"], this.toggleMode.bind(this, -1));
+    Mousetrap.bind(["ctrl+right", "meta+right"], this.toggleMode.bind(this, 1));
+  }
+
+  toggleMode(gotoDirection) {
+    let timeTypes = this.getFormatTypes();
+    let currentPosition = -1;
+
+    for (let i = 0; i < timeTypes.length; i++) {
+      if (timeTypes[i].time === this.state.timeType) {
+        currentPosition = i;
+        break;
+      }
+    }
+
+    if (currentPosition !== -1) {
+      let newMode = timeTypes[currentPosition + gotoDirection];
+      if (newMode) this.setTime(newMode.time);
+    }
+  }
+
+  _setLocalStorage(item, element) {
+    let value = element.target.checked;
+    localStorage.setItem("react-pomodoro-" + item, value);
+  }
+
+  _getLocalStorage(item) {
+    return localStorage.getItem("react-pomodoro-" + item) == "true"
+      ? true
+      : false;
+  }
+
+  alert() {
+    // vibration
+    if (this.refs.vibrate.checked) {
+      window.navigator.vibrate(1000);
+    }
+    // audio
+    if (this.refs.audio.checked) {
+      let audio = new Audio("songs/alarm.mp3");
+      audio.play();
+      setTimeout(() => audio.pause(), 1400);
+    }
+    // notification
+    if (this.refs.notification.checked) {
+      if (this.state.timeType === 1500) {
+        let notification = new Notification("Relax :)", {
+          icon: "img/coffee.png",
+          lang: "en",
+          body: "Go talk or drink a coffee.",
+        });
+      } else {
+        let notification = new Notification("The time is over!", {
+          icon: "img/code.png",
+          lang: "en",
+          body: "Hey, back to code!",
+        });
+      }
+    }
+  }
+
+  capitalize(s) {
+    if (typeof s !== "string") return "";
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header title="Pomodoro" sound alert />
+
+        <CounterContainer>
+          <CircularProgressbarWithChildren
+            maxValue={1500}
+            strokeWidth={3}
+            value={this.state.time}
+            styles={buildStyles({
+              rotation: 1,
+              strokeLinecap: "round",
+              pathTransitionDuration: 0.5,
+              pathColor: "#7457f2",
+              textColor: "#f88",
+              trailColor: "#3c405e",
+              backgroundColor: "#7457f2",
+            })}
+          >
+            <TimeNum>{this.format(this.state.time)}</TimeNum>
+            <br />
+            <TimeText>Minutes</TimeText>
+          </CircularProgressbarWithChildren>
+        </CounterContainer>
+
+        <ContentContainer>
+          <ContentTitle>
+            {this.capitalize(this.formatType(this.state.timeType))}
+          </ContentTitle>
+          <ActionButtonsContainer type="work">
+            <ActionButton active />
+            <ActionButton />
+            <ActionButton />
+          </ActionButtonsContainer>
+        </ContentContainer>
+
+        <PlayStopContent>
+          <SquareButton
+            onClick={this.state.play === true ? this.reset : this.play}
+            play={!this.state.play}
+          />
+        </PlayStopContent>
+      </Container>
+    );
+  }
 }
-
-export default Main;
